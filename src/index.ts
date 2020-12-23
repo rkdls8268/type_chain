34ab0f82e403eb92161565f4b9b99bcd24614742
rkdls8268 <rkdls8268@gmail.com>
2020-12-24 02:06:14 +0900

    interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-// const name = "Gain",
-//     age = 25,
-//     gender = "female";
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
 
-const sayHi = (name:string, age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const person = {
+    name: "Gain",
+    gender: "female",
+    age: 25
 };
 
-// sayHi(name, age, gender);
-console.log(sayHi("Gain", 25, "female"));
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
