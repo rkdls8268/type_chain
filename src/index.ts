ac0d568f735f9c1cb613af578be134c3f6b93cc3
rkdls8268 <rkdls8268@gmail.com>
2020-12-24 02:33:11 +0900

    classes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+class Human {
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    // get 키워드 사용해서 getter 함수 역할
+    get getAge() {
+        return this.age;
+    }
+    // set 키워드 사용해서 setter 함수 역할
+    set setAge(age2) {
+        this.age = age2;
+    }
 }
+
+const lala = new Human("Lala", 25, "female");
 
 const person = {
     name: "Gain",
@@ -11,9 +26,13 @@ const person = {
 };
 
 const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+    console.log(`Current age: ${person.getAge}`);
+    // setAge는 함수가 아니므로 setAge()와 같이 호출하면 안됨.
+    person.setAge = 20;
+
+    return `Hello ${person.name}, you are ${person.getAge}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lala));
 
 export {};
